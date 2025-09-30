fd20c6eee36a15203438f1c573c0f42015cf227e
Michael Kit <captkit@ukr.net>
2025-09-30 12:57:13 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import css from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <h2 className={css.header}>Header</h2>
+        </div>
+    </>
+  );
+}
